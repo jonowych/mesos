824e42554a64673da8a6c69a735b87c43e33ef7b
jonowych <jonowych@gmail.com>
2017-09-21 23:45:44 +1000

    script update

--- cluster-config.sh
+++ cluster-config.sh
@@ -21,28 +21,44 @@ intf=$(ifconfig | grep -m1 ^e | awk '{print $1 }')
 oldhost=$(hostname)
 oldip=$(ifconfig | grep $intf -A 1 | grep inet | awk '{ print $2 }' | awk -F: '{ print $2 }')
 
-# set up zookeeper connection info
-echo -n "zk://"  > zk-temp
+# set up mesos connection info for all nodes
+if [ -z $(which mesos) ] ; then
+   echo "!!$(tput setaf 1) /etc/mesos/zk is not updated !!"
+   echo "!! because mesos is not installed in node !!"
+   echo $(tput sgr0) && exit ; fi
+
+echo -n "zk://"  > /etc/mesos/zk
+for (( k=$new; k<`expr $new + $size - 1`; k++))
+do
+   newip=$(echo $oldip | cut -d. -f4 --complement).$k
+   echo -n "$newip:2181," >> /etc/mesos/zk
+done
+   k=`expr $new + $size - 1`
+   newip=$(echo $oldip | cut -d. -f4 --complement).$k
+   echo "$newip:2181/mesos" >> /etc/mesos/zk
+   echo "$(tput setaf 6)!! /etc/mesos/zk has been updated. !!$(tput sgr0)"
+   echo
+
+# set up zookeeper connection info for master nodes
+if [ -z $(which marathon) ] ; then
+   echo "!!$(tput setaf 1) /etc/zookeeper/conf/zoo.cfg is not updated !!"
+   echo "because marathon is not installed in node !!"
+   echo $(tput sgr0) && exit ; fi
 
 for (( k=$new; k<`expr $new + $size - 1`; k++))
 do
    newip=$(echo $oldip | cut -d. -f4 --complement).$k
-   echo -n "$newip:2181," >> zk-temp
    echo "server.$k=zookeeper$k:2888:3888" >> zoo-temp
 done
    k=`expr $new + $size - 1`
    newip=$(echo $oldip | cut -d. -f4 --complement).$k
-   echo "$newip:2181/mesos" >> zk-temp
    echo "server.$k=zookeeper$k:2888:3888" >> zoo-temp
 
 # import zookeeper connection info to system config files.
-cat zk-temp > /etc/mesos/zk
 
 k=`expr $(awk '/.2888.3888/{print NR;exit}' /etc/zookeeper/conf/zoo.cfg) - 1`
-sed -i '/.2888.3888/d' /etc/zookeeper/conf/zoo.cfg
-sed -i "$k r zoo-temp" /etc/zookeeper/conf/zoo.cfg
-
-rm zk-temp zoo-temp
+sed -i -e '/.2888.3888/d' -e "$k r zoo-temp" /etc/zookeeper/conf/zoo.cfg
+rm zoo-temp
 
 # set up quorum for over 50 percent of the master members in cluster
 let "size = size/2 +size%2"
