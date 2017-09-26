ae5d12a9af6128e489496fc1f28940c3a715020c
jonowych <jonowych@users.noreply.github.com>
2017-09-26 23:03:18 +1000

    Update mesos-install.sh

--- mesos-install.sh
+++ mesos-install.sh
@@ -22,15 +22,45 @@ CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
 apt-get -y update
 
+# Get existing IP information
+echo
+read -p "How many nodes in Mesosphere cluster: " size
+read -p "Enter first node number in cluster: " new
+
+if ! [ $new -eq $new ] 2>/dev/null ; then
+        echo "$(tput setaf 1)!! Exit -- Sorry, integer only !!$(tput sgr0)"
+        exit ; fi
+if [ -z $new ] || [ $new -lt 1 ] || [ $new -gt 254 ] ; then
+        echo "$(tput setaf 1)!! Exit -- node number out of range !!$(tput sgr0)"
+        exit ; fi
+
+new=$(echo $new | sed 's/^0*//')
+intf=$(ifconfig | grep -m1 ^e | awk '{print $1 }')
+
+oldhost=$(hostname)
+oldip=$(ifconfig | grep $intf -A 1 | grep inet | awk '{ print $2 }' | awk -F: '{ print $2 }')
+
 # Although mesos includes zookeeper, it requires downgraded versions of libevent-dev & libssl-dev.
 # Those downgraded packages will break zookeeper daeman as "active (exited)".
 # 
 # Install zookeeperd before mesos to work around the problem.
-apt-get -y install zookeeperd
 
-# Install Mesosphere packages
+# ------------
 echo "$(tput setaf 3)!! Installing zookeeper !!$(tput sgr0)"
 apt-get -y install zookeeperd
+
+# set up zookeeper connection info in a temporary file
+for (( k=$new; k<`expr $new + $size`; k++))
+do
+   newip=$(echo $oldip | cut -d. -f4 --complement).$k
+   echo "server.$k=zookeeper$k:2888:3888" >> zoo-temp
+done
+
+# import zookeeper connection info to system config files.
+k=`expr $(awk '/.2888.3888/{print NR;exit}' /etc/zookeeper/conf/zoo.cfg) - 1`
+sed -i -e '/.2888.3888/d' -e "$k r zoo-temp" /etc/zookeeper/conf/zoo.cfg
+rm zoo-temp
+
 systemctl start zookeeper
 systemctl enable zookeeper
 
@@ -38,7 +68,7 @@ systemctl enable zookeeper
 echo "$(tput setaf 3)!! Installing mesos=$mesos_ver !!$(tput sgr0)"
 apt-get -y install mesos="$mesos_ver"
 
-cat <<EOF_mesos > mesos-master.service
+cat <<EOF_mesos > /etc/systemd/system/mesos-master.service
 [Unit]
 Description=Mesos Master Service
 After=zookeeper.service
@@ -51,16 +81,15 @@ ExecStart=/usr/local/sbin/mesos-master
 WantedBy=multi-user.target
 EOF_mesos
 
-   sudo mv mesos-master.service /etc/systemd/system/
-   sudo systemctl daemon-reload
-   sudo systemctl start mesos-master.service
-   sudo systemctl enable mesos-master
+systemctl daemon-reload
+systemctl start mesos-master.service
+systemctl enable mesos-master
 
 # ------------
 echo "$(tput setaf 3)!! Installing marathon=$marathon_ver !!$(tput sgr0)"
 apt-get -y install marathon="$marathon_ver"
 
-cat <<EOF_marathon > marathon.service
+cat <<EOF_marathon > /etc/systemd/system/marathon.service
 [Unit]
 Description=Marathon Service
 After=mesos-master.service
@@ -73,7 +102,6 @@ ExecStart=/usr/local/bin/marathon
 WantedBy=multi-user.target
 EOF_marathon
 
-mv mesos-master.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start marathon.service
 systemctl enable marathon.service
@@ -82,7 +110,7 @@ systemctl enable marathon.service
 echo "$(tput setaf 3)!! Installing chronos !!$(tput sgr0)"
 apt-get -y install chronos
    
-cat <<EOF_chronos > chronos.service
+cat <<EOF_chronos > /etc/systemd/system/chronos.service
 [Unit]
 Description=Chronos Service
 After=marathon.service
@@ -95,7 +123,6 @@ ExecStart=/usr/local/bin/chronos
 WantedBy=multi-user.target
 EOF_chronos
 
-mv chronos.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start chronos.service
 systemctl enable chronos.service
