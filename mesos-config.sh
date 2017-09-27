003dca71425698fadff00f5b4b21e7744000792c
jonowych <jonowych@users.noreply.github.com>
2017-09-27 23:16:37 +1000

    Update mesos-config.sh

--- mesos-config.sh
+++ mesos-config.sh
@@ -12,14 +12,7 @@ if [ -z $(which mesos) ] ; then
 intf=$(ifconfig | grep -m1 ^e | awk '{print $1}')
 oldhost=$(hostname)
 oldip=$(ifconfig | grep $intf -A 1 | grep inet | awk '{print $2}' | awk -F: '{print $2}')
-new=$(echo $oldip | awk -F. '{print $4}')
-sed -i "s/127.0.1.1/$oldip/" /etc/hosts
-
-echo
-echo "$(tput setaf 6)!! Update $1 node name from $oldhost to $newhost !!"
-echo "!! Update node IP from $oldip to $newip !! $(tput sgr0)"
-echo && echo System will restart in 10 seconds
-sleep 10
+ID=$(echo $oldip | awk -F. '{print $4}')
 
 systemctl stop chronos.service
 systemctl stop marathon.service
@@ -30,7 +23,7 @@ systemctl stop zookeeper.service
 first=$(cat /etc/mesos/cluster | awk -F, '{print $1}')
 size=$(cat /etc/mesos/cluster | awk -F, '{print $2}')
 
-if [ $new -lt $first ] || [ $new -ge `expr $first + $size` ] ; then
+if [ $ID -lt $first ] || [ $ID -ge `expr $first + $size` ] ; then
    echo "!! Updating Mesosphere $(tput setaf 6)slave configuration$(tput sgr0) !!"
    systemctl disable chronos.service
    systemctl disable marathon.service
