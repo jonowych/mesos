b7df9a1c9e1608435062cdcc8bfc75cb79105795
jonowych <jonowych@users.noreply.github.com>
2017-09-27 17:22:29 +1000

    Update mesos-config.sh

--- mesos-config.sh
+++ mesos-config.sh
@@ -8,7 +8,7 @@ if [ -z $(which mesos) ] ; then
    echo "!!$(tput setaf 1) mesos is not installed !!"
    echo $(tput sgr0) && exit ; fi
 
-echo && read -p "Please enter host node number: " new
+echo && read -p "Please enter new host node number: " new
 
 if ! [ $new -eq $new ] 2>/dev/null ; then
         echo -e "$(tput setaf 1)!! Exit -- Sorry, integer only !!$(tput sgr0)"
@@ -17,30 +17,33 @@ if [ -z $new ] || [ $new -lt 1 ] || [ $new -gt 254 ] ; then
         echo "$(tput setaf 1)!! Exit -- node number out of range !!$(tput sgr0)"
         exit 1; fi
 
-new=$(echo $new | sed 's/^0*//')
+# Get exisitng IP address and host name
 intf=$(ifconfig | grep -m1 ^e | awk '{print $1 }')
-
 oldhost=$(hostname)
 oldip=$(ifconfig | grep $intf -A 1 | grep inet | awk '{ print $2 }' | awk -F: '{ print $2 }')
 
-read -p "Change hostname? [enter] for no change: " newhost
-if [ -z $newhost ]
-   then newhost=$(echo $oldhost | cut -d- -f1)-$new
-   else newhost=$newhost-$new ; fi
-
+new=$(echo $new | sed 's/^0*//')
+newhost=$(echo $oldhost | cut -d- -f1)-$new
 newip=$(echo $oldip | cut -d. -f4 --complement).$new
+
+first=$(cat /etc/mesos/cluster | awk -F, '(print $1}'
+size=$(cat /etc/mesos/cluster | awk -F, '(print $2}'
+
+if [ $new -lt $first ] || [ $new -gt `expr $first + $size` ] ; then echo "slave" ; fi 
 
 echo
 echo "$(tput setaf 6)!! Update $1 node name from $oldhost to $newhost !!"
 echo "!! Update node IP from $oldip to $newip !! $(tput sgr0)"
 echo && echo System will restart in 10 seconds
+
+exit
 sleep 10
 
 sed -i "s/$oldhost/$newhost/" /etc/hostname
-sed -i "s/$oldhost/$newhost/" /etc/hosts
+sed -i -e "s/$oldhost/$newhost/" -e "s/$oldip/$newip/" /etc/hosts
 sed -i "s/$oldip/$newip/" /etc/network/interfaces
 
-if [ -z $(which marathon) ] ; then
+if [ $new -lt $first ] || [ $new -gt `expr $first + $size` ] ; then
    # update mesos and zookeeper data
    service zookeeper stop	# slave does not run zookeeper
    echo manual > /etc/init/zookeeper.override
