f2b1c4b525c92f1fbeef1619304ef2efd54f38a4
jonc-techdata <jonc-techdata@outlook.com>
2017-10-04 12:21:48 +1100

    keep single mesos-install.sh

--- mesos-install.sh
+++ mesos-install.sh
@@ -5,30 +5,43 @@ if [ ! "${USER}" = "root" ] ; then
    echo "!! Please enter command as root $(tput setaf 1)sudo $0 $(tput sgr0)!!"
    echo && exit ; fi
 
-if [ -e /etc/systemd/system/mesos-master.service ] ; then
-	echo $(tput setaf 2)
-	echo "!! Exit - This is a Master node which already has mesos package installed!!"
+if [ -e /etc/systemd/system/mesos-master.service ] ; then mesos=master
+elif [ -e /etc/systemd/system/mesos-slave.service ] ; then mesos=slave
+elif [ ! -e /etc/apt/sources.list.d/mesosphere.list ] ; then mesos=new
+else	echo $(tput setaf 1)
+	echo "!! Error -- mesosphere.list exits but cannot find"
+	echo "neither mesos-master.service nor mesos-slave.service"
 	echo $(tput sgr0) && exit
-elif [ -e /etc/systemd/system/mesos-slave.service ] ; then
-	echo $(tput setaf 2)
-	echo "!! Exit - This is a Slave node which already has mesos package installed!!"
-	echo $(tput sgr0) && exit
-else
-	echo "$(tput setaf 3)How many master nodes in mesosphere cluster?"
-	read -p "Enter [1-9] to install master, none to install slave: " size
-	echo $(tput sgr0)
-
-	if ! [ $size -eq $size ] 2>/dev/null ; then
-		echo $(tput setaf 1)
-		echo "!! Exit -- Sorry, integer only !!"
-		echo $(tput sgr0) && exit
-	elif [ -z $size ] ; then mesos=slave
-	elif [ $size -ge 1 ] && [ $size -le 9 ] ; then mesos=master
-	else	echo $(tput setaf 1)
-		echo "!! Exit -- Please enter cluster size between 1 and 9 !!"
-		echo $(tput sgr0) && exit
-	fi
 fi
+
+echo $(tput setaf 3)
+echo "How many master nodes in mesosphere cluster?"
+echo "Enter [0] to install slave; or [1-9] to install master;"
+read -p "Enter none to update IP in mesos-master start-up only: " size 
+echo $(tput sgr0)
+
+if [ -z $size ] ; then 
+        if [ $mesos = "master" ] ; then mesos=update
+	else	echo $(tput setaf 1)
+		echo "!! No update because this node is $mesos !!" 
+		echo $(tput sgr0) && exit
+	fi 
+elif ! [ $size -eq $size ] 2>/dev/null ; then
+        echo $(tput setaf 1)
+        echo "!! Exit -- Sorry, integer only !!"
+        echo $(tput sgr0) && exit
+elif [ $mesos="new" ] && [ $size -ge 1 ] && [ $size -le 9 ] ; then mesos=master
+elif [ $mesos="new" ] && [ $size -eq 0 ] ; then mesos=slave
+else
+        echo $(tput setaf 1)
+        echo "!! Exit -- Please enter cluster size between 0 and 9 !!"
+        echo $(tput sgr0) && exit
+fi
+
+echo "Mesos package will be installed in this node."
+echo "If already installed, mesos configuration will be updated."
+echo "Action=$mesos, press Ctl-C if not proceed."
+echo && sleep 10
 
 # Get system IP information
 intf=$(ifconfig | grep -m1 ^e | awk '{print $1}')
@@ -36,17 +49,34 @@ syshost=$(hostname)
 sysip=$(ifconfig | grep $intf -A 1 | grep inet | awk '{print $2}' | awk -F: '{print $2}')
 sysnode=$(echo $sysip | awk -F. '{print $4}')
 
+if [ $mesos = "update" ] ; then
+	echo $(tput setaf 6)
+	echo "!! Update mesos-master.service with system IP."
+	echo $(tput sgr0)
+
+	#get zookeeper IP information 
+	zoonode=$(cat /etc/zookeeper/conf/myid)
+	zooip=$(echo $sysip | cut -d. -f4 --complement).$zoonode
+	echo $sysnode > /etc/zookeeper/conf/myid
+	# Update mesos-master.service
+	sed -i "s/=$zooip/=$sysip/g" mesos-master.service
+
+	echo && echo "!! Warning - System will restart in 10 seconds ........"
+	sleep 10
+	shutdown -r now
+fi
+
 if [ $mesos = "slave" ] ; then
 	# Get mesosphere cluster configuration from master node
-	
+
 	echo "$(tput setaf 6)!! This node will be installed as mesos-slave !!"
 	echo "Contact Master node to retrieve cluster configuration;$(tput sgr0)"
 	read -p "Enter mesosphere master node number (single number): " k
-		
+
 	masterip=$(echo $sysip | cut -d. -f4 --complement).$k
 
 	ping -q -c3 $masterip > /dev/null
-		if [ $? -eq 0 ] ; then 
+		if [ $? -eq 0 ] ; then
 			scp sydadmin@$masterip:/etc/mesos/zk /tmp/
 		else
 			echo -n $(tput setaf 1)
@@ -55,15 +85,17 @@ if [ $mesos = "slave" ] ; then
 		fi
 fi
 
-# Add GPG key for the official mesosphere repository and update
-apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
-# Add mesosphere repository to APT sources
-DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
-# Add repository according linux distro
-CODENAME=$(lsb_release -cs)
-echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" \
-   | sudo tee /etc/apt/sources.list.d/mesosphere.list
-apt-get -y update
+if [ $mesos = "new" ] ; then
+	# Add GPG key for the official mesosphere repository and update
+	apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+	# Add mesosphere repository to APT sources
+	DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+	# Add repository according linux distro
+	CODENAME=$(lsb_release -cs)
+	echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" \
+		| sudo tee /etc/apt/sources.list.d/mesosphere.list
+	apt-get -y update
+fi
 
 if [ $mesos = "master" ] ; then
 	echo "$(tput setaf 3)!! Installing zookeeper package !!$(tput sgr0)"
@@ -96,8 +128,10 @@ slave)
 cat <<EOF_mesos > /etc/systemd/system/mesos-slave.service
 [Unit]
    Description=Mesos Slave Service
+
 [Service]
    ExecStart=/usr/sbin/mesos-slave --master=$(cat /tmp/zk) --work_dir=/var/lib/mesos
+
 [Install]
    WantedBy=multi-user.target
 EOF_mesos
@@ -127,8 +161,10 @@ cat <<EOF_mesos > /etc/systemd/system/mesos-master.service
    Description=Mesos Master Service
    After=zookeeper.service
    Requires=zookeeper.service
+
 [Service]
    ExecStart=/usr/sbin/mesos-master --ip=$sysip --hostname=$sysip --zk=$(cat /etc/mesos/zk) --quorum=$k --work_dir=/var/lib/mesos
+
 [Install]
    WantedBy=multi-user.target
 EOF_mesos
@@ -152,8 +188,10 @@ cat <<EOF_marathon > /etc/systemd/system/marathon.service
    Description=Marathon Service
    After=mesos-master.service
    Requires=mesos-master.service
+
 [Service]
    ExecStart=/usr/bin/marathon --master $(cat /etc/mesos/zk) --zk $(cat /etc/mesos/zk | sed 's/mesos/marathon/')
+
 [Install]
    WantedBy=multi-user.target
 EOF_marathon
@@ -173,8 +211,10 @@ cat <<EOF_chronos > /etc/systemd/system/chronos.service
    Description=Chronos Service
    After=marathon.service
    Requires=marathon.service
+
 [Service]
    ExecStart=/usr/bin/chronos
+
 [Install]
    WantedBy=multi-user.target
 EOF_chronos
